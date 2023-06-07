daeb2d9ac94e6ffb6c7261aaf16a0f857e621caa
TheSebasx11 <sebabalcar18@gmail.com>
2023-06-07 18:06:33 -0500

    changes on index

--- index.py
+++ index.py
@@ -1,9 +1,23 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager
+from views.assist_view import Assists_View
 
-class ExampleApp(App):
+
+
+
+class MyApp(App):
     def build(self):
-        return Label(text="Hola, mundo!")
+        scManager = ScreenManager()
+        ass_view = Assists_View(name = "assist_view")
+
+        scManager.add_widget(ass_view)
+
+        scManager.current = "assist_view"
+
+        return scManager
+    
+
 
 if __name__ == "__main__":
-    ExampleApp().run()
+    MyApp().run()
---
+++ views/assist_view.py
@@ -0,0 +1,10 @@
+import kivy.uix.boxlayout
+from kivy.uix.label import Label
+import kivy.uix.anchorlayout
+from kivy.uix.screenmanager import Screen
+
+class Assists_View(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.add_widget(Label(text = "hola"))
+
