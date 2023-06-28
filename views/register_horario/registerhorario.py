ef9c9a3eaeea1f4eab8004938c78e8e70fb064a3
Josepxd2022 <100246773+Josepxd2022@users.noreply.github.com>
2023-06-27 22:41:22 -0500

    creo que sirve jaja

--- views/register_horario/registerhorario.py
+++ views/register_horario/registerhorario.py
@@ -4,27 +4,33 @@ from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.spinner import Spinner
 
 currentFilePath = os.path.dirname(os.path.abspath(__file__))
-kv = Builder.load_file(os.path.join(currentFilePath,"registerhorarios.kv"))
+kv = Builder.load_file(os.path.join(currentFilePath, "registerhorarios.kv"))
 
 
 class RegisterHorariosLayout(BoxLayout):
-    def increase_number(self, number):
-        print(f"Increasing number: {number}")
+    def registrar_horario(self):
+        hora_llegada = self.ids.hora_llegada_spinner.text
+        minutos_llegada = self.ids.minutos_llegada_spinner.text
+        hora_salida = self.ids.hora_salida_spinner.text
+        minutos_salida = self.ids.minutos_salida_spinner.text
+
+        horario_string = f'Horario de llegada: {hora_llegada}:{minutos_llegada}\nHorario de salida: {hora_salida}:{minutos_salida}'
+        print(horario_string)  # Aquí puedes hacer lo que necesites con el string de horario
+
 
 class RegisterHorarios(Screen):
-     def __init__(self, **kw):
+    def __init__(self, **kw):
         super().__init__(**kw)
         self.add_widget(RegisterHorariosLayout())
 
 
-"""class RegisterHorarios(App):
+class MyApp(App):
     def build(self):
-        return RegisterHorariosLayout()
-"""
-if __name__ == '__main__':
-    RegisterHorarios().run()
+        return RegisterHorarios()
+
+
+MyApp().run()
